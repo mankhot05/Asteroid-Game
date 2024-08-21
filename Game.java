2ba12c37579d85d52a9f578786c2bdaa1ed03f68
mankhot05 <142852845+mankhot05@users.noreply.github.com>
2024-08-21 12:21:49 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -16,33 +16,29 @@ public class Game extends JComponent implements KeyListener {
 
 	private int coordX = 390;
 	private int coordY = 500;
+	private int ascoordY = 0;
+	private int ascoordX = 0;
 	private int dx = 0;
 	private int dy = 0;
 	private Image image;
-	private long lastAsteroidSpawnTime = 0;
-	private double time = 0.01; 
+	private long lastAsteroidSpawnTime = 0; 
 	private LinkedList<Asteroid> asteroids = new LinkedList<>();
-	private LinkedList<Shot> shots = new LinkedList<>();
-    private int numberofiterations = 0;
-	private int points = 0;
-	private boolean printGameOver = false;
-	private static JFrame frame;
 	
 	public Game() {
 		setPreferredSize(new Dimension(800, 640));
-        setFocusable(true); // Allows the panel to receive keyboard input
-        addKeyListener(this); // Register the panel as a listener for key events
+        	setFocusable(true); // Allows the panel to receive keyboard input
+        	addKeyListener(this); // Register the panel as a listener for key events
 
 
-        // Game thread
-        Thread modelThread = new Thread(new GameLogic());
-        modelThread.start();
+	        // Game thread
+	        Thread modelThread = new Thread(new GameLogic());
+	        modelThread.start();
 	}
 	
 	public void paintComponent(Graphics g) {
-		
+		/* 
 		loadImage();
-	
+		*/
 		super.paintComponent(g);
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
@@ -51,35 +47,23 @@ public class Game extends JComponent implements KeyListener {
 		Graphics2D graphics = (Graphics2D) g;
 		graphics.drawImage(image, coordX, coordY, this);
 		
-		/* 
+		
 		g.setColor(Color.WHITE);
 		g.fillRect(coordX, coordY, 20, 20); 
-		*/
 
 		for (Asteroid asteroid: asteroids) {
-			
 			g.setColor(Color.WHITE);
-			g.fillOval(asteroid.getXcoord(), asteroid.getYcoord(), asteroid.getWidth(), asteroid.getHeight());
-			
-		}
-
-		for (Shot shot: shots) {
-			g.setColor(Color.WHITE);
-			g.fillRect(shot.getXCoordShot(), shot.getYCoordShot(), 5, 10);
-		}
-
-		if (printGameOver) {
-			g.setColor(Color.RED);
-            g.setFont(new Font("Arial", Font.BOLD, 50));
-            g.drawString("Game Over", getWidth() / 2 - 150, getHeight() / 2);
+			g.drawOval(asteroid.getXcoord(), asteroid.getYcoord(), asteroid.getWidth(), asteroid.getHeight());
 		}
 	}
 
-	
+	/* 
 	public void loadImage() {
 		ImageIcon imageicon = new ImageIcon("C:\\Users\\mvenk\\OneDrive\\Desktop\\Visual Studio Code\\Asteroid Game\\resources\\spaceship image.png");
 		image = imageicon.getImage();
 	}
+	*/
+	
 	public class GameLogic implements Runnable {
 
 		private boolean alive = true;
@@ -88,73 +72,21 @@ public class Game extends JComponent implements KeyListener {
 		public void run() {
 			while (alive) {
 				charmove();
-				shotsmove();
-                createAsteroid();
-                for (Asteroid asteroid: asteroids) {
-                    asteroid.moveAsteroid();
-                }
 				for (Asteroid asteroid: asteroids) {
-					for (Shot shot: shots) {
-						collision(asteroid, shot);
-					}
+					asteroid.moveAsteroid();
 				}
-
-				// Game over checkers
-				for (Asteroid asteroid: asteroids) {
-
-					int asteroidLeft = asteroid.getXcoord();
-					int asteroidRight = asteroid.getXcoord() + asteroid.getWidth();
-					int asteroidTop = asteroid.getYcoord();
-					int asteroidBottom = asteroid.getYcoord() + asteroid.getHeight();
-
-					int charLeft = coordX;
-					int charRight = coordX + 20; 
-					int charTop = coordY;
-					int charBottom = coordY + 20; 
-
-					boolean collisionX = charRight >= asteroidLeft && charLeft <= asteroidRight;
-					boolean collisionY = charBottom >= asteroidTop && charTop <= asteroidBottom;
-
-					if (asteroid.getYcoord() == 640) {
-						alive = false;
-						printGameOver = true;
-					}
-					else if (collisionX && collisionY) {
-						alive = false;
-						printGameOver = true;
-					}
-				}
-				
-
-				// Removing asteroids and shots after collision
-				Iterator<Asteroid> iterator = asteroids.iterator();
-				Asteroid asteroid;
-				while(iterator.hasNext()) {
-					asteroid = iterator.next();
-					if(asteroid.isMarked()) {
-						iterator.remove();
-						points += 100;
-					}
-				}
-
-				Iterator<Shot> iteratorshot = shots.iterator();
-				Shot shot;
-				while(iteratorshot.hasNext()) {
-					shot = iteratorshot.next();
-					if (shot.isMarked()) {
-						iteratorshot.remove();
-					}
+				asteroids.removeIf(asteroid -> asteroid.getYcoord() < 0);
+				if (System.currentTimeMillis() - lastAsteroidSpawnTime > 1000) {
+					createAsteroid();
+					lastAsteroidSpawnTime = System.currentTimeMillis();
 				}
 
 				repaint();
-
-
 				try {
 					Thread.sleep(16);
 				} catch (InterruptedException  e) {
 					e.printStackTrace();
 				}
-                numberofiterations += 1;
 			}
 		}
 	}
@@ -164,19 +96,25 @@ public class Game extends JComponent implements KeyListener {
 		private int ycoord;
 		private int width;
 		private int height;
-		private boolean remove;
 
 		public Asteroid() {
 			boolean numberTaken = false;
 			Random random = new Random();
-			this.xcoord = random.nextInt(750);
-			this.ycoord = 0;
-			this.width = random.nextInt(65) + 25;
+			while (!numberTaken) {
+				for (Asteroid asteroid: asteroids) {
+					if (asteroid.getXcoord() != xcoord) {
+						numberTaken = false;
+						this.xcoord = random.nextInt(800);
+					}
+				}
+			}
+			this.ycoord = 640;
+			this.width = random.nextInt(100);
 			this.height = width;
 		}
 
 		public void moveAsteroid() {
-			ycoord += 1;
+			ycoord -= 15;
 		}
 
 		public int getXcoord() {
@@ -193,46 +131,6 @@ public class Game extends JComponent implements KeyListener {
 
 		public int getHeight() {
 			return this.height;
-		}
-
-		public void setMarked(boolean mark) {
-			remove = mark;
-		}
-
-		public boolean isMarked() {
-			return remove;
-		}
-	}
-
-	public class Shot {
-
-		private int xcoord;
-		private int ycoord = coordY;
-		private boolean remove;
-
-		public Shot(int xcoord) {
-			this.xcoord = xcoord;
-			repaint();
-		}
-
-		public int getXCoordShot() {
-			return xcoord;
-		}
-
-		public void setYCoordShot(int y) {
-			ycoord = y;
-		}
-
-		public int getYCoordShot() {
-			return ycoord;
-		}
-
-		public void setMarked(boolean mark) {
-			remove = mark;
-		}
-
-		public boolean isMarked() {
-			return remove;
 		}
 	}
 	
@@ -257,9 +155,6 @@ public class Game extends JComponent implements KeyListener {
 		else if (key == KeyEvent.VK_DOWN) {
 			dy += 15;
 		}
-		else if (key == KeyEvent.VK_SPACE) {
-			shots.add(new Shot(coordX));
-		}
 	}
 	
 	public void charmove() {
@@ -269,58 +164,27 @@ public class Game extends JComponent implements KeyListener {
 		dy = 0;
 	}
 
-	public void shotsmove() {
-		for (Shot shot: shots) {
-			shot.setYCoordShot(shot.getYCoordShot() - 5);
-		}
-	}
-
 	public int randomxcoord() {
 		Random random = new Random();
 		return random.nextInt(800);
 	}
 
 	public void createAsteroid() {
-		long currentTime = System.currentTimeMillis();
-        long elapsedTime = currentTime - lastAsteroidSpawnTime;
-        
-        int spawnDelay = 2000; 
-        System.out.println(time);
-
-        if (elapsedTime > spawnDelay) {
-            asteroids.add(new Asteroid());
-            lastAsteroidSpawnTime = currentTime;
-        }
+		asteroids.add(new Asteroid());
 	}
 
-	public void collision(Asteroid asteroid, Shot shot) {
-		int asteroidLeft = asteroid.getXcoord();
-		int asteroidRight = asteroid.getXcoord() + asteroid.getWidth();
-		int asteroidTop = asteroid.getYcoord();
-		int asteroidBottom = asteroid.getYcoord() + asteroid.getHeight();
-
-		int shotLeft = shot.getXCoordShot();
-		int shotRight = shot.getXCoordShot() + 5; 
-		int shotTop = shot.getYCoordShot();
-		int shotBottom = shot.getYCoordShot() + 10; 
-
-		boolean collisionX = shotRight >= asteroidLeft && shotLeft <= asteroidRight;
-		boolean collisionY = shotBottom >= asteroidTop && shotTop <= asteroidBottom;
-
-		if (collisionX && collisionY) {
-			asteroid.setMarked(true);
-			shot.setMarked(true);
-		}
+	public void removeAsteroid() {
+		
 	}
 	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
-			frame = new JFrame("Asteroid");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(new Game());
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+			JFrame frame = new JFrame("Asteroid");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.getContentPane().add(new Game());
+			frame.pack();
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
 		});
 	}
 }
